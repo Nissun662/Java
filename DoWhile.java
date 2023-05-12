c342adb253a65a0d61abbe16e369ab8b6b98b47b
Nishan Sunar <97493559+Nissun662@users.noreply.github.com>
2023-05-12 17:49:11 -0500

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+    public static void main(String a[]) 
+    {
+        int i = 3;
+
+        do 
+        {
+            System.out.println("Hi " + 1);
+
+            i++;
+        }while(i <= 4);
+    }
+}
---
+++ Forloop.java
@@ -0,0 +1,14 @@
+public class Forloop {
+    public static void main(String[] args) 
+    {
+        for(int i = 1; i <=5; i++) 
+        {
+            System.out.println("Day" + i);
+
+            // for(int j = 9; j < 18; j++)
+            // {
+            //     System.out.println( "   " + j+" am" + " - " + (j+1)+" pm");
+            // }
+        }
+    }
+}
